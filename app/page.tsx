03cb38546fa23876381b079856350b239177c200
Mirindra <lantosoamirindra@gmail.com>
2025-07-27 21:17:00 +0300

    homepage2

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       />
       <div className="absolute inset-0 w-full h-screen bg-arriere bg-opacity-80"/>
       <div className="absolute top-1/2 left1/2 w-full">
-        <h2 className="text-l text-center">Hello, I'm Mirindra</h2>
+        <h2 className="text-l text-center">{"Hello, I'm Mirindra"}</h2>
         <h1 className="text-xl sm:text-3xl text-center">Fullstack web developer<br/>{"\u0026"}<br/>Electronics engineer and automation</h1>
       </div>
     </div>
