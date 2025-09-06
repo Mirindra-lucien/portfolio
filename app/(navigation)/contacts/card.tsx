1989d34c8df1bd00090ce7afcca0bd5a35462860
Mirindra <lantosoamirindra@gmail.com>
2025-09-06 15:56:04 +0300

    add new button

---
+++ app/(navigation)/contacts/card.tsx
@@ -0,0 +1,10 @@
+import { MapPin, Mail, Home, Phone } from "lucide-react";
+
+export default function Card(){
+    return (<div>
+        <MapPin className="text-blue"/>
+        <Mail/>
+        <Home/>
+        <Phone/>
+    </div>);
+}
