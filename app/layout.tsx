7d2ad890323169da72a2e1c6c4b595fe1319b323
Mirindra <lantosoamirindra@gmail.com>
2025-12-01 19:17:31 +0300

    adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,22 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Mirindra",
-  description: "Fullstack web developer",
-  icons: {icon: "/icon.png"}
+  title: "Mirindra Portfolio - Fullstack developer",
+  description: "Explore my projects in web development, real-time applications, and comprehensive technical solutions using Javascript, React, Vue, Node, Symfony and Laravel",
+  keywords: "Mirindra, portfolio, web developer, full stack, web project",
+  authors: {name: "LANTOSOA Mirindra Lucien"},
+  robots: "index, follow",
+  icons: {icon: "/icon.png"},
+  openGraph: {
+    title: "Mirindra Portfolio - Fullstack Developer",
+    description: "Explore my web projects and comprehensive technical solutions",
+    url: "https://mirindra.vercel.app",
+    siteName: "Mirindra",
+    images: [{
+      url: "/og_img.jpg"
+    }],
+    type: "website"
+  }
 };
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
