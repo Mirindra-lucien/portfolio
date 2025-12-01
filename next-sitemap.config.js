3c095113781b02c2af06a8a02f5766d5ef64b836
Mirindra <lantosoamirindra@gmail.com>
2025-12-01 19:49:08 +0300

    adding SEO files

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type
+{import('next-sitemap').IConfig}*/
+module.exports = {
+    siteUrl: "https://mirindra.vercel.app",
+    generateRobotsTxt: true,
+    generateIndexSitemap: true,
+    changefreq: 'daily',
+    priority: 0.7
+};
