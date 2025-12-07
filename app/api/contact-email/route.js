83283bd77fc962095a38531c2883427dc59f1e37
Mirindra <lantosoamirindra@gmail.com>
2025-12-07 16:43:55 +0300

    full version

--- app/api/contact-email/route.js
+++ app/api/contact-email/route.js
@@ -1,9 +1,9 @@
 import { Resend } from "resend";
 import {EmailTemplate} from "./emailTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
+export async function POST(req) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     try {
         const {name, email, message} = await req.json();
 
